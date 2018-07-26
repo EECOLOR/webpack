0e17f124d1b827990b063ef94e5c64daa4c702b9
Tobias Koppers <tobias.koppers@googlemail.com>
2018-07-26 16:51:02 +0200

    bugfix/empty-chunks-fail

---
+++ test/cases/chunks/nested-in-empty/index.js
@@ -0,0 +1,11 @@
+it("should include a chunk nested in an empty chunk", (done) => {
+	require.ensure(["./a"], () => {
+		require.ensure([], () => {
+			require.ensure([], () => {
+				const b = require("./b");
+				expect(b).toBe(42);
+				done();
+			});
+		});
+	});
+});
